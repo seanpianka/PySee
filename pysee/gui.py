46646ca0bedbc3a5ba6febbc8b804d11d3613fe4
SeanPianka <me@seanpianka.com>
2016-03-16 17:40:01 -0400

    Updated GUI

--- pysee/gui.py
+++ pysee/gui.py
@@ -19,21 +19,51 @@ from pysee import run
 class Application(tk.Frame):
     def __init__(self):
         self.root = tk.Tk()
-        # self.root.geometry("640x480+8000+750")
-        self.root.title("PySee")
+        #self.root.geometry("520x480+8000+750")
+        self.root.title("PySee, a Lightweight Screenshot Tool")
         
-        # label creation
-        self.lbl_maintitle = tk.Label(self.root, \
-                                      text="PySee, a Lightweight Screenshot Tool")
-        self.lbl_imghosttitle = tk.Label(self.root, \
-                                         text="Select your image host:")
-        self.lbl_configtitle = tk.Label(self.root, \
-                                         text = "Edit your configuration file:")
+        # f_master: frame atop of root
+        self.f_master = tk.Frame(self.root,
+                                 bg = 'white',
+                                 width = 520,
+                                 height = 400)
+        self.f_master.pack(fill="both", expand=True)
+        # f_imghost: frame for right side/img host selection
+        self.f_imghost = tk.Frame(self.f_master,
+                                  bg = 'lightsalmon', 
+                                  height=200,
+                                  width=260).grid(row=0,
+                                                  column=0)
+        # f_config: frame for left side/config editor
+        self.f_config = tk.Frame(self.f_master,
+                                 bg = 'purple',
+                                 height=200,
+                                 width=260).grid(row=0,
+                                                 column=1)
 
-        # packing
+        # label creation
+        self.lbl_maintitle = tk.Label(self.f_master, \
+                                      text="PySee")
+        self.lbl_imghosttitle = tk.Label(self.f_imghost, \
+                                         text="Select your image host:")
+        self.lbl_configtitle = tk.Label(self.f_config, \
+                                        text = "Edit your config:")
+        self.lbl_maintitle.config(bg='lightgreen',
+                                  font=('Monospace', 24),
+                                  pady=10,
+                                  padx=50)
         self.lbl_maintitle.pack()
-        self.lbl_imghosttitle.pack(side="left")
-        self.lbl_configtitle.pack(side="right")
+        self.lbl_imghosttitle.config(bg='lightgreen',
+                                  font=('Monospace', 24),
+                                  pady=10,
+                                  padx=50)
+        self.lbl_imghosttitle.place(in_=self.f_imghost)
+
+        self.lbl_configtitle.config(bg='lightgreen',
+                                  font=('Monospace', 24),
+                                  pady=10,
+                                  padx=50)
+        self.lbl_configtitle.place(in_=self.f_config)
 
         # init main loop
         self.root.mainloop()
