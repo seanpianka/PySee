c1336a48cc05c37fe946620ecdc1839c4263a934
Sean Pianka <pianka@eml.cc>
2016-03-21 22:26:26 -0400

    Updated GUI

--- pysee/gui.py
+++ pysee/gui.py
@@ -19,52 +19,58 @@ from pysee import run
 class Application(tk.Frame):
     def __init__(self):
         self.root = tk.Tk()
-        #self.root.geometry("520x480+8000+750")
+        self.root.geometry("520x480+8000+750")
         self.root.title("PySee, a Lightweight Screenshot Tool")
         
         # f_master: frame atop of root
         self.f_master = tk.Frame(self.root,
-                                 bg = 'white',
-                                 width = 520,
-                                 height = 400)
-        self.f_master.pack(fill="both", expand=True)
+                                 bg='white',
+                                 width=520,
+                                 height=500)
+        self.f_title = tk.Frame(self.f_master,
+                                bg='blue',
+                                width=520,
+                                height=100)
         # f_imghost: frame for right side/img host selection
         self.f_imghost = tk.Frame(self.f_master,
-                                  bg = 'lightsalmon', 
-                                  height=200,
-                                  width=260).grid(row=0,
-                                                  column=0)
+                                  bg='lightsalmon', 
+                                  height=400,
+                                  width=260)
+                                            
         # f_config: frame for left side/config editor
         self.f_config = tk.Frame(self.f_master,
-                                 bg = 'purple',
-                                 height=200,
-                                 width=260).grid(row=0,
-                                                 column=1)
+                                 bg='purple',
+                                 height=400,
+                                 width=260)
 
         # label creation
-        self.lbl_maintitle = tk.Label(self.f_master, \
-                                      text="PySee")
+        self.lbl_maintitle = tk.Label(self.f_title, \
+                                      text="PySee",
+                                      bg='lightgreen',
+                                      font=('Monospace', 24),
+                                      pady=10,
+                                      padx=50)
         self.lbl_imghosttitle = tk.Label(self.f_imghost, \
-                                         text="Select your image host:")
+                                         text="Select your image host:",
+                                         bg='lightgreen',
+                                         font=('Monospace', 16),
+                                         pady=10,
+                                         padx=50)
         self.lbl_configtitle = tk.Label(self.f_config, \
-                                        text = "Edit your config:")
-        self.lbl_maintitle.config(bg='lightgreen',
-                                  font=('Monospace', 24),
-                                  pady=10,
-                                  padx=50)
-        self.lbl_maintitle.pack()
-        self.lbl_imghosttitle.config(bg='lightgreen',
-                                  font=('Monospace', 24),
-                                  pady=10,
-                                  padx=50)
-        self.lbl_imghosttitle.place(in_=self.f_imghost)
+                                        text="Edit your config:",
+                                        bg='lightgreen',
+                                        font=('Monospace', 16),
+                                        pady=10,
+                                        padx=50)
 
-        self.lbl_configtitle.config(bg='lightgreen',
-                                  font=('Monospace', 24),
-                                  pady=10,
-                                  padx=50)
-        self.lbl_configtitle.place(in_=self.f_config)
+        self.lbl_maintitle.pack(expand=True, side='top')
+        self.lbl_imghosttitle.pack(expand=True, side='right')
+        self.lbl_configtitle.pack(expand=True, side='left')
 
+        self.f_title.pack(expand=True, side='top')
+        self.f_imghost.pack(expand=True)
+        self.f_config.pack(eexpand=Truexpand=True, side='left')
+        self.f_master.pack()
         # init main loop
         self.root.mainloop()
 
