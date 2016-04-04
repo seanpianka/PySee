39cb9e5e99a9944e35ab4665c958edd7f9090b02
Sean Pianka <pianka@eml.cc>
2016-04-03 20:44:53 -0400

    Added option for uploading to uploads.im image host

--- pysee/uploads_im.py
+++ pysee/uploads_im.py
@@ -8,5 +8,18 @@ for uploading to Uploads.im.
 :author: Sean Pianka <me@seanpianka.com>
 :copyright: Copyright 2016 Sean Pianka
 :license: None
-
 """
+
+import requests
+import json
+
+UPLOADS_API_URL = "http://uploads.im/api"
+
+
+def upload_picture(image_path):
+    with open(image_path['path'], 'rb') as img:
+        response = requests.post(UPLOADS_API_URL, files={'upload':img})
+    if response.json()['status_code'] is 200:
+        return response.json()['data']
+    else:
+        return None
