fab164afa8208c7bae707fef2d2ed22bb283b00f
seanpianka <me@seanpianka.com>
2016-04-04 00:24:32 -0400

    no real changes besides .gitignore

--- pysee/tk2gui.py
+++ pysee/tk2gui.py
@@ -10,63 +10,65 @@ host to upload to.
 :author: Sean Pianka <me@seanpianka.com>
 :copyright: Copyright 2016 Sean Pianka
 :license: None
-
 """
-import tkinter as tk
 
-from pysee import main
+import tkinter as tk
+from tkinter import Button, Label, Frame, OptionMenu, StringVar
+
+from pysee import take_screenshot
 from helpers import edit_text
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+        Frame.__init__(self, master)
         self.master.title("PySee, a Lightweight Screenshot Tool")
 
-        self.f_main = tk.Frame(master)
-        self.f_imghost = tk.Frame(master)
-        self.f_config = tk.Frame(master)
+        self.f_main = Frame(master)
+        self.f_imghost = Frame(master)
+        self.f_config = Frame(master)
 
-        self.lbl_maintitle = tk.Label(self.f_main, \
-                                      text="PySee",
-                                      font=('Monospace', 24))
-        self.lbl_imghosttitle = tk.Label(self.f_imghost, \
-                                         text="Select your image host:",
-                                         font=('Monospace', 10))
-        self.lbl_configtitle = tk.Label(self.f_config, \
-                                        text="Edit your config:",
-                                        font=('Monospace', 10))
+        self.lbl_main = Label(self.f_main,
+                              text="PySee",
+                              font=('Monospace', 24))
+        self.lbl_imghost = Label(self.f_imghost,
+                                 text="Select your image host:",
+                                 font=('Monospace', 10))
+        self.lbl_config = Label(self.f_config,
+                                text="Edit Configuration File:",
+                                font=('Monospace', 10))
 
-        self.image_host = tk.StringVar()
+        self.image_host = StringVar()
         self.image_host.set("Select Image Host")
-        self.optn_imghost = tk.OptionMenu(self.f_imghost,
-                                          self.image_host, "Imgur", "Minus",
-                                          "Photobucket", "PostImage")
-        self.btn_editconfig = tk.Button(self.f_config,
-                                      text="Edit Configuration File",
-                                      command=lambda:edit_text("~/.pysee/config.ini"))
-        self.btn_takescreenshot = tk.Button(self.f_main,
-                                            text="Take a Screenshot",
-                                            command=main)
-
-        # binding keys
-        self.btn_takescreenshot.bind('<Return>', main)
-        self.btn_takescreenshot.bind('<space>', main)
+        self.optn_imghost = OptionMenu(self.f_imghost,
+                                       self.image_host,
+                                       "Imgur.com",
+                                       "Uploads.im")
+        self.btn_editconfig = Button(self.f_config,
+                                     text="Open in Text Editor",
+                                     command=lambda:
+                                      edit_text("~/.pysee/config.ini"))
+        self.btn_screenshot = Button(self.f_main,
+                                     text="Take a Screenshot",
+                                     command=lambda:
+                                      take_screenshot(root=master,
+                                                      image_host="I",
+                                                      clipboard=True))
 
         # packing
-        self.lbl_maintitle.pack(fill="x")       # PySee title
+        self.lbl_main.pack(fill="x")  # PySee title
 
-        self.lbl_configtitle.pack()             # Config title
-        self.btn_editconfig.pack()              # Edit Config button
+        self.lbl_config.pack()        # Config title
+        self.btn_editconfig.pack()    # Edit Config button
 
-        self.lbl_imghosttitle.pack()            # Image Host title
-        self.optn_imghost.pack()                # Image Host dropdown menu
+        self.lbl_imghost.pack()       # Image Host title
+        self.optn_imghost.pack()      # Image Host dropdown menu
 
-        self.btn_takescreenshot.pack()          # Take screenshot button
+        self.btn_screenshot.pack()    # Take screenshot button
 
-        self.f_main.pack()                      # Title frame
-        self.f_imghost.pack()                   # Image Host frame
-        self.f_config.pack()                    # Edit Config frame
+        self.f_main.pack()            # Title frame
+        self.f_imghost.pack()         # Image Host frame
+        self.f_config.pack()          # Edit Config frame
 
 
 def create_gui():
