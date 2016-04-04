de93b48f77d67926ee121c04cfc9c3cd3c5b3624
Sean Pianka <pianka@eml.cc>
2016-04-03 20:44:27 -0400

    Refactored response handling, code clean-up and readability improvement, more PEP8 validation

--- pysee/pysee.py
+++ pysee/pysee.py
@@ -9,8 +9,8 @@ Imgur uploading and system clipboard copying.
 :author: Sean Pianka <me@seanpianka.com>
 :copyright: Copyright 2016 Sean Pianka
 :license: None
-
 """
+
 import os
 import errno
 import sys
@@ -20,11 +20,11 @@ import pyperclip
 import imgur
 from datetime import datetime as dt
 
+import uploads_im
 from configs import paths as p, verify_configuration
 from helpers import find_screenshot_tool
 
-
-error_msg = "There was an error when attempting to save" +
+error_msg = "There was an error when attempting to save" + \
             "or upload the screenshot. Please try again."
 
 
@@ -38,49 +38,43 @@ def capture_screenshot(tool, image_path):
     cmd.communicate()
 
     img_name = img_path.replace(p['imgdir'], '')[:-4]
-    image_path['path'] = img_path # absolute path of screenshot 
-    image_path['name'] = img_name # filename of screenshot 
+    image_path['path'] = img_path  # absolute path of screenshot
+    image_path['name'] = img_name  # filename of screenshot
+
+    return None
 
 
-def upload_screenshot(image_host):
-    if image_host is "Imgur.com":
+def upload_screenshot(image_host, image_path):
+    if image_host is "Imgur" or image_host is "I":
         response = imgur.upload_picture(image_path)
         image_url = response['link']
-    elif image_host is "Uploads.im":
-        img_file = open(image_paths['path'], 'rb')
-        response = pass
-        image_url = pass
+    elif image_host is "Uploads" or image_host is "U":
+        response = uploads_im.upload_picture(image_path)
+        image_url = response['img_url']
 
-    return {
-        'status_code': status_code,
-        'image_url': image_url
-    }
+    return image_url
 
 
-
-def take_screenshot(event=None, root=None, image_host="1"):
+def take_screenshot(event=None, root=None, image_host="U"):
     verify_configuration()
 
-    tool = find_screenshot_tool()
+    screenshot_tool = find_screenshot_tool()
     image_path = {}
+    capture_screenshot(screenshot_tool, image_path)
+    image_url = upload_screenshot(image_host, image_path)
 
-    capture_screenshot(tool, image_path)
-    response = upload_screenshot(image_host)
-
-    try:
-        if response is None:
-            raise requests.exceptions.RequestException
+    if image_url is not None and image_url is not "":
         pyperclip.copy(image_url)
         print("Successful upload of {}.png!".format(image_path['name']),
               "\nYou can find it here: {}".format(image_url),
               "\nIt has also been copied to your system clipboard.")
-    except requests.exceptions.RequestException as e:
-        print("There was an error: ", e)
-        sys.exit(2)
-    except:
+        upload_success = True
+    else:
+        upload_success = False
         print(error_msg)
-        sys.exit(1)
+
+    return upload_success
 
 
 if __name__ == "__main__":
-    main()
+    take_screenshot()
